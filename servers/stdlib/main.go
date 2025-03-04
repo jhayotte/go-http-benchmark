4a8e9a28d95e0d5c0422b9e4cd4934f85b5ed6c6
Julien Hayotte <julien.hayotte@gmail.com>
2025-03-04 08:49:04 +0100

    fix: add missing error check
    Signed-off-by: Julien Hayotte <julien.hayotte@gmail.com>

--- servers/stdlib/main.go
+++ servers/stdlib/main.go
@@ -11,5 +11,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic("cannot listen and serve on 8080")
+	}
 }
